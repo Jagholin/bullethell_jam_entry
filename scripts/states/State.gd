8ec97c0bbffc875cf12f28a9459fbdd5aa0ac2ad
Jagholin <pavel.belskiy2@gmail.com>
2024-05-13 13:27:51 +0200

    cleanup

--- scripts/states/State.gd
+++ scripts/states/State.gd
@@ -3,6 +3,8 @@ class_name State
 
 var animation_name: String = ""
 
+@export var animated_sprite: AnimatedSprite2D
+
 var target: Node2D
 var state_expire_timeout: float = -1
 var state_expiration_ticks: int = -1
@@ -11,9 +13,9 @@ var active: bool = false
 
 func enter() -> void:
 	print("enter into base state")
-	var sprite := target.get_node("AnimatedSprite2D") as AnimatedSprite2D
-	if sprite != null:
-		sprite.animation = get_animation_name()
+	# var sprite := target.get_node("AnimatedSprite2D") as AnimatedSprite2D
+	if animated_sprite != null:
+		animated_sprite.animation = get_animation_name()
 	elapsed = 0.0
 	active = true
 
