9c7eb267e88b1c3aa77eab5cff00cac14663edf9
Josep Valls <josep@valls.name>
2024-05-14 23:33:07 -0700

    basic powerups

---
+++ scripts/resources/item_settings_resource.gd
@@ -0,0 +1,6 @@
+class_name ItemSettingsResource
+extends Resource
+
+enum ItemType { MORE_VOLLEYS, FASTER_BULLETS }
+## The type of bullet this is, either an enemy bullet or a player bullet.
+@export var item_type: ItemType
