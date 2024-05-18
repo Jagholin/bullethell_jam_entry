a7ce719d4067bcc66e0e8f335713b9ee04263b04
Jagholin <pavel.belskiy2@gmail.com>
2024-05-18 15:26:14 +0200

    add main menu

--- scripts/player_provider.gd
+++ scripts/player_provider.gd
@@ -1,18 +1,22 @@
 extends Node
 
 var player: Player:
-    set(newValue):
-        if player == newValue:
-            return
-        player = newValue
-        player_changed.emit(player)
+	set(newValue):
+		if player == newValue:
+			return
+		player = newValue
+		if not player:
+			player_destroyed.emit()
+		else:
+			player_changed.emit(player)
 
 signal player_changed(player: Player)
+signal player_destroyed
 
 func on_player_initialized(cb: Callable):
-    if not player:
-        await player_changed
-    cb.call(player)
+	if not player:
+		await player_changed
+	cb.call(player)
 
 func set_player(new_player: Player):
-    player = new_player
+	player = new_player
--- scripts/scenes/main.gd
+++ scripts/scenes/main.gd
@@ -1,7 +1,43 @@
-extends BaseLevel
+extends Node2D
+
+@onready var main_menu_layer: CanvasLayer = $MainMenuLayer
+@onready var pause_menu_layer: CanvasLayer = $PauseMenuLayer
+
+@export var level_scenes: Array[PackedScene]
+
+var current_level: BaseLevel
+var current_level_idx: int
 
 func _ready():
-	super._ready()
+	main_menu_layer.show()
+	pause_menu_layer.hide()
 
 func _process(_delta):
-	super._process(_delta)
+	pass
+
+## Creates level with idx = current_level_idx and adds it as a child to this node
+func start_level():
+	var scene := level_scenes[current_level_idx]
+	var newLevel := scene.instantiate() as BaseLevel
+	current_level = newLevel
+	add_child(current_level)
+
+func on_level_exit():
+	current_level.queue_free()
+	current_level = null
+	main_menu_layer.show()
+
+func _on_new_game_button_pressed():
+	assert(not current_level)
+	main_menu_layer.hide()
+	current_level_idx = 0
+	start_level()
+
+func _on_credits_button_pressed():
+	pass # Replace with function body.
+
+func _on_exit_button_pressed():
+	# If we are on web, quitting this way won't work
+	if Engine.get_architecture_name().contains("wasm"):
+		return
+	get_tree().quit()
